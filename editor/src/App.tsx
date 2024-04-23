3a71710d32d68c2cb83f27197ee7a929e9a54930
Jardan Andrei <jardan.andrei@gmail.com>
2024-04-23 11:11:11 +0300

    commit before separating out CommandLine from App

--- editor/src/App.tsx
+++ editor/src/App.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import CodeMirror from '@uiw/react-codemirror';
+import CodeMirror, { Decoration, DecorationSet, EditorView, GutterMarker, ReactCodeMirrorRef, StateEffect, StateField, gutter } from '@uiw/react-codemirror';
 import { indentUnit } from '@codemirror/language'
 import axios from 'axios';
 import { BsPlayFill, BsFillStopFill } from "react-icons/bs";
 
 const endpoint = "localhost:8000";
+
+const emptyMarker = new class extends GutterMarker {
+  toDOM() {
+    const node = document.createElement("span");
+    node.innerText = "●";
+    node.style.color = "red";
+    return node;
+  }
+}
+
+const addLineHighlight = StateEffect.define();
+
+const lineHighlightMark = Decoration.line({
+  attributes: {style: 'background-color: #d2ffff'},
+});
 
 function App() {
   const instance = axios.create({
@@ -13,10 +28,131 @@ function App() {
   });
 
   const [jobId, setJobId] = React.useState<null|number>(null);
+  const [error, setError] = React.useState<null|any>(null);
   const [socket, setSocket] = React.useState<null|WebSocket>(null);
   const running = socket !== null;
 
+  const [valueCode, setValueCode] = React.useState("");
+  const onChangeCode = React.useCallback((val: any, viewUpdate: any) => {
+    setValueCode(val);
+  }, []);
+
+  const editorRef = React.useRef<null|ReactCodeMirrorRef>(null);
+  const emptyLineGutter = gutter({
+    lineMarker(view, line) {
+      console.log(error);
+      if (error === null) return null;
+      const lineNumber = view.state.doc.lineAt(line.from).number;
+      if (lineNumber !== error.line) return null;
+      return emptyMarker;
+    },
+    initialSpacer: () => emptyMarker
+  });
+
+  // const lineHighlightField = StateField.define({
+  //   create() {
+  //     return Decoration.none;
+  //   },
+  //   update(lines, tr) {
+  //     lines = lines.map(tr.changes);
+  //     for (let e of tr.effects) {
+  //       if (e.is(addLineHighlight)) {
+  //         lines = Decoration.none;
+  //         lines = lines.update({add: [lineHighlightMark.range(e.value)]});
+  //       }
+  //     }
+  //     return lines;
+  //   },
+  //   provide: (f) => EditorView.decorations.from(f),
+  // });
+
+  React.useEffect(() => {
+    console.log(error);
+    if (error === null) return;
+    setValueOutput(error.message);
+    // let effects = [];
+    // effects.push(addUnderline.of({from: 1, to: 10}));
+    // effects.push(StateEffect.appendConfig.of([emptyLineGutter]))
+    // editorRef.current?.view?.dispatch({effects});
+  }, [error, valueCode]);
+  
   const inputRef = React.useRef<null|HTMLDivElement>(null);
+
+  function ensureSelection() {
+    const input = inputRef.current;
+    if (input === null) return;
+
+    const firstChild = input.firstChild;
+    if (firstChild === null) {
+      console.warn("invalid state");
+      return;
+    }
+    const textLength = firstChild.nodeValue?.length;
+    if (textLength === undefined) {
+      console.warn("invalid state");
+      return;
+    }
+
+    let range = new Range();
+    range.setStart(firstChild, textLength);
+    range.setEnd(firstChild, textLength);
+
+    const selection = document.getSelection();
+    selection?.removeAllRanges();
+    selection?.addRange(range);
+  }
+
+  function ensureInputText() {
+    const input = inputRef.current;
+    if (input === null) return;
+
+    let text = "";
+    for (const node of Array.from(input.childNodes)) {
+      if (node.nodeType === document.TEXT_NODE) {
+        if (node.textContent !== null) {
+          text = text.concat(node.textContent);
+        }
+      }
+      node.remove();
+    }
+
+    text = text.replaceAll("\n", "");
+    const node = document.createTextNode("");
+    node.nodeValue = text;
+    input.appendChild(node);
+  }
+
+  function ensureInputCursor() {
+    const input = inputRef.current;
+    if (input === null) return;
+
+    const cursors = input.getElementsByClassName("cursor");
+    if (cursors.length !== 1) {
+      if (cursors.length > 1) {
+        for (const cursor of Array.from(cursors)) {
+          cursor.remove();
+        }
+      }
+
+      const node = document.createElement("span");
+      node.textContent = "█";
+      node.classList.add("cursor");
+      node.style.userSelect = "none";
+
+      let before = input.firstChild?.nextSibling;
+      if (before === undefined) before = null;
+      
+      input.insertBefore(node, before);
+    }
+  }
+
+  React.useEffect(() => {
+    ensureInputText();
+    ensureInputCursor();
+  }, []);
+  
+  const outputRef = React.useRef<null|HTMLDivElement>(null);
+  const [valueOutput, setValueOutput] = React.useState("");
 
   React.useEffect(() => {
     if (jobId === null && socket !== null) {
@@ -29,6 +165,7 @@ function App() {
       socket.addEventListener("message", (event) => {
         const data = JSON.parse(event.data);
         setValueOutput((valueOutput) => valueOutput + data.stdout);
+
       });
       socket.addEventListener("close", (_event) => {
         setJobId(null);
@@ -38,16 +175,12 @@ function App() {
     }
   }, [jobId]);
 
-  const [valueCode, setValueCode] = React.useState("");
-  const onChangeCode = React.useCallback((val: any, viewUpdate: any) => {
-    setValueCode(val);
-  }, []);
+  React.useEffect(() => {
+    const output = outputRef.current;
+    if (output === null) return;
+    output.scrollTo(0, output.scrollHeight);
+  }, [valueOutput]);
 
-  const [valueOutput, setValueOutput] = React.useState("");
-  const onChangeOutput = React.useCallback((val: any, viewUpdate: any) => {
-    setValueOutput(val);
-  }, []);
-  
   return (
     <div
       style={{
@@ -80,15 +213,25 @@ function App() {
           onClick={async () => {
             setValueOutput("");
             setJobId(null);
+            setError(null);
+
+            // const code = editorRef.current?.state?.doc.toString();
+            // const code = editorRef.current?.editor?.nodeValue;
+            console.log(editorRef);
+            console.log(editorRef.current?.state);
             
             const response = await instance.post("/job", {code: valueCode});
             const data = response.data;
 
-            console.log(response);
-            if ("job_id" in data) {
-              setJobId(data.job_id);
-            } else {
-              setValueOutput(data.value)
+            switch (data.message.type) {
+              case "success": {
+                setJobId(data.message.job_id);
+                break;
+              }
+              case "failure": {
+                setError(data.message.error)
+                break;
+              }
             }
           }}
           style={{
@@ -123,7 +266,10 @@ function App() {
         <div
           style={{
             flexGrow: 1,
+            flexShrink: 1,
+            flexBasis: 0,
 
+            maxHeight: "100%",
             display: "flex",
             flexDirection: "column",
 
@@ -148,11 +294,16 @@ function App() {
               }}
               height="100%"
 
-              value={valueCode}
+              ref={editorRef}
+
               extensions={[
                 indentUnit.of("\t"),
+                emptyLineGutter,
+                // lineHighlightField,
               ]}
               indentWithTab={true}
+
+              value={valueCode}
               onChange={onChangeCode}
             />
           </div>
@@ -160,69 +311,115 @@ function App() {
         <div
           style={{
             flexGrow: 1,
+            flexShrink: 1,
+            flexBasis: 0,
 
+            maxHeight: "100%",
             display: "flex",
             flexDirection: "column",
+            overflowY: "auto",
 
             backgroundColor: "#f5f5f5",
           }}
         >
-          <div>output {running ? "(running)" : ""}</div>
+          <div>console {running ? "(running)" : ""}</div>
           <div
             style={{
-              height: "100%",
-              width: "100%",
-              position: "relative",
+              paddingLeft: "6px",
+              paddingRight: "2px",
+              paddingTop: "4px",
+              paddingBottom: "4px",
+              backgroundColor: "white",
+              fontFamily: "monospace",
+
+              display: "flex",
+              flexDirection: "column",
+
+              overflowY: "auto",
+            }}
+            onClick={(_event) => {
+              if (inputRef.current === null) return;
+              inputRef.current.focus();
             }}
           >
-            <CodeMirror
+            <div
+              ref={outputRef}
               style={{
-                height: "100%",
-                width: "100%",
-                position: "absolute",
+                whiteSpace: "break-spaces",
+                overflowY: "auto",
               }}
-              height="100%"
+            >{valueOutput}</div>
+            <div
+              ref={inputRef}
+              spellCheck={false}
+              contentEditable={true}
+              style={{
+                flexGrow: 1,
+                flexBasis: "auto",
+                
+                caretColor: "transparent",
+                padding: "0",
+                border: "0",
+                width: "100%",
+                minHeight: "2ch",
+                whiteSpace: "break-spaces",
+              }}
+              onSelect={(event) => {
+                event.preventDefault();
+              }}
+              onBeforeInput={(_event) => {
+                ensureInputText();
+                ensureSelection();
+              }}
+              onInput={(event) => {
+                ensureInputText();
+                ensureSelection();
+                ensureInputCursor();
+              }}
+              onKeyDown={(event) => {
+                if (
+                    event.key === "ArrowUp"
+                  || event.key === "ArrowDown"
+                  || event.key === "ArrowLeft"
+                  || event.key === "ArrowRight"
+                ) {
+                  event.preventDefault();
+                }
+                
+                if (event.key === "Enter") {
+                  event.preventDefault();
+                  if (socket === null) {
+                    return;
+                  }
+                  const input = inputRef.current;
+                  if (input === null) {
+                    return;
+                  }
 
-              value={valueOutput}
-              onChange={onChangeOutput}
-              editable={false}
+                  const firstChild = input.firstChild;
+                  if (firstChild === null) {
+                    console.warn("invalid state");
+                    return;
+                  }
+                  const text = firstChild.nodeValue;
+                  if (text === undefined) {
+                    console.warn("invalid state");
+                    return;
+                  }
+                  socket.send(JSON.stringify({
+                    message: {
+                      type: "stdin",
+                      stdin: `${text}\n`,
+                    }
+                  }));
+
+                  firstChild.nodeValue = "";
+                }
+              }}
             />
           </div>
         </div>
-        <div
-          style={{
-            width: "100%",
-            border: "0",
-          }}
-        >
-          <div
-            contentEditable={true}
-            ref={inputRef}
-            style={{
-            }}
-            onKeyDown={(event) => {
-              if (event.key === "Enter") {
-                event.preventDefault();
-                if (socket === null) {
-                  console.warn("socket is null when it shouldn't be");
-                  return;
-                }
-                if (inputRef.current === null) {
-                  return;
-                }
-
-                const text = inputRef.current.innerText;
-                socket.send(JSON.stringify({
-                  message: {
-                    type: "stdin",
-                    stdin: `${text}\n`,
-                  }
-                }));
-              }
-            }}
-          />
         </div>
-      </div>
     </div>
 
     </div>
---
+++ editor/src/CommandLine.tsx
@@ -0,0 +1,12 @@
+interface CommandLineProps {
+  
+}
+
+const CommandLine: React.FunctionComponent<CommandLineProps> = (props) => {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default CommandLine;
